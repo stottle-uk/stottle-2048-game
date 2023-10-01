1c99de48c135a06e6e59b12f526eb35a3a6304dd
stottle-uk <stuarttottle@hotmail.com>
2023-10-01 19:52:05 -0300

    added firebase

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,7 +1,17 @@
+import { initializeApp } from 'firebase/app';
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-
 import App from './app/App';
+
+initializeApp({
+  apiKey: 'AIzaSyAon5sU3HnLBOff-qtHTjXiyUrRp7DuTFI',
+  authDomain: 'game-ce466.firebaseapp.com',
+  projectId: 'game-ce466',
+  storageBucket: 'game-ce466.appspot.com',
+  messagingSenderId: '617341184113',
+  appId: '1:617341184113:web:56ab89edb2f57469bb93d0',
+  measurementId: 'G-33SSRBMY0K',
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,5 @@
-import styles from './App.module.scss';
 import Board from './game/Board';
 
-export const App: React.FC = () => {
-  return (
-    <div className={styles['stuff']}>
-      <Board />
-    </div>
-  );
-};
+export const App: React.FC = () => <Board />;
 
 export default App;
