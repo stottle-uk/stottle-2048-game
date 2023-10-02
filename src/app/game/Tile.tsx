aa0cbbb63a512bc19abf40b53451942262c7ecfa
stottle-uk <stuarttottle@hotmail.com>
2023-10-02 17:14:38 -0300

    added tile animation

--- src/app/game/Tile.tsx
+++ src/app/game/Tile.tsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useLayoutEffect, useRef } from 'react';
 
-interface OwnProps extends Omit<React.HTMLProps<HTMLDivElement>, 'className'> {
+interface OwnProps
+  extends Omit<React.HTMLProps<HTMLDivElement>, 'className' | 'ref'> {
   value: number;
 }
 
-const Tile: React.FC<OwnProps> = ({ value, ...props }) => (
-  <div data-testid="tile" className={`tile tile-${value}`} {...props}>
-    {value > 0 ? <span>{value}</span> : <span>&nbsp;</span>}
-  </div>
-);
+const Tile: React.FC<OwnProps> = ({ value, ...props }) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    if (ref.current && value > 0) {
+      const tile = ref.current;
+      tile.classList.add('tile-change');
+      setTimeout(() => {
+        tile.classList.remove('tile-change');
+      }, 100);
+    }
+  }, [value]);
+
+  return (
+    <div
+      data-testid="tile"
+      className={`tile tile-${value}`}
+      ref={ref}
+      {...props}
+    >
+      {value > 0 ? <span>{value}</span> : <span>&nbsp;</span>}
+    </div>
+  );
+};
 
 export default Tile;
